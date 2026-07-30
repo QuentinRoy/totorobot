4257f6cb457a71a7659444e743f47aece2f50037
Quentin Roy <quentin@quentinroy.fr>
2026-07-30 18:46:35 +0200

    Adapt Robot3 action test to Totorobot

--- tests/robot3/test-action.js
+++ tests/robot3/test-action.js
@@ -1,21 +1,21 @@
-import { createMachine, action, interpret, state, transition } from 'totorobot';
+import { defineMachine, interpret } from 'totorobot';
 
 QUnit.module('Action', () => {
   QUnit.test('Can be used to do side-effects', assert => {
     let count = 0;
     let orig = {};
-    let machine = createMachine({
+    let machine = defineMachine().create('one', ({ action, state, transition }) => ({
       one: state(
         transition('ping', 'two',
           action(() => count++)
         )
       ),
       two: state()
-    }, () => orig);
-    let service = interpret(machine, () => {});
-    service.send('ping');
+    }));
+    let service = interpret(machine, orig);
+    service.send({ type: 'ping' });
 
-    assert.equal(service.context, orig, 'context stays the same');
+    assert.equal(service.snapshot.context, orig, 'context stays the same');
     assert.equal(count, 1, 'side-effect performed');
   });
 });
